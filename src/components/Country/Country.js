a1859c3605019a3f2f56c91373b3bb16e5464227
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-21 20:15:37 +0600

    more info added to country

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,13 +2,15 @@ import "./Country.css";
 import React from "react";
 
 const Country = props => {
-  const { name, capital, flags } = props.country;
+  const { name, capital, flags, area, continents } = props.country;
 
   return (
     <div className="country">
       <h1>Name: {name.common}</h1>
       <img src={flags.png} alt="flag" />
       <h3>Capital: {capital}</h3>
+      <h3>Area: {area} sq. km</h3>
+      <h3>Continent: {continents}</h3>
     </div>
   );
 };
